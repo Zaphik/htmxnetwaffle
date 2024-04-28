bc4a9d6f35f2b53e9fbab5bd85aada0e91c01450
Zaphik <142690769+Zaphik@users.noreply.github.com>
2024-04-28 16:39:57 +0100

    Initial commit

---
+++ SSR/Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+        .ToArray();
+
+    var html = forecast.Aggregate("<h1>Weather Forecast</h1><ul>", (current, weather) => current + $"<li>Date: {weather.Date}, Temperature: {weather.TemperatureC}C, Summary: {weather.Summary}</li>");
+    html += "</ul>";
+
+    return Results.Content(html, "text/html");
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/", () => Results.Content("<h1>HTMX with ASP.NET Core</h1>", "text/html"))
+    .WithName("Home")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
